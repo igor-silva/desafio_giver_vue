abb3dfdf22ca58f25c48e81e60440877c4e40803
igor-silva <igor-d-silva@hotmail.com>
2022-10-06 18:04:15 -0300

    Update Services

--- src/service/CustomerService.js
+++ src/service/CustomerService.js
@@ -11,7 +11,7 @@ export default class CustomerService {
 
     async getCustomersIsValidEmail() {
 
-        const response = await fetch("http://appigor.epizy.com/php/api/5/1/999999999");
+        const response = await fetch("http://appigor.epizy.com/customer/api/5/1/999999999");
         const data = await response.json();
         this.data = data;        
 
@@ -20,7 +20,7 @@ export default class CustomerService {
 
     async getCustomersNotIsValidEmail() {
 
-        const response = await fetch("http://appigor.epizy.com/php/api/4/1/999999999");
+        const response = await fetch("http://appigor.epizy.com/customer/api/4/1/999999999");
         const data = await response.json();
         this.data = data;        
 
@@ -29,7 +29,7 @@ export default class CustomerService {
 
     async getCustomersTotal() {
 
-        const response = await fetch("http://appigor.epizy.com/php/api/8/1/999999999");
+        const response = await fetch("http://appigor.epizy.com/customer/api/8/1/999999999");
         const data = await response.json();
         this.data = data;        
 
@@ -39,7 +39,7 @@ export default class CustomerService {
 
     async getCustomersGender() {
 
-        const response = await fetch("http://appigor.epizy.com/php/api/9/1/999999999");
+        const response = await fetch("http://appigor.epizy.com/customer/api/9/1/999999999");
         const data = await response.json();
         this.data = data;        
 
@@ -49,7 +49,7 @@ export default class CustomerService {
 
     async getCustomersIsLastName() {
 
-        const response = await fetch("http://appigor.epizy.com/php/api/3/1/999999999");
+        const response = await fetch("http://appigor.epizy.com/customer/api/3/1/999999999");
         const data = await response.json();
         this.data = data;        
 
